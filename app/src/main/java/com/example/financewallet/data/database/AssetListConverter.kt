dc7d857073ceaef45641eb75fef62553cad2f76e
John <muravei20004@mail.ru>
2024-08-16 14:19:59 +0300

    libs fix

--- app/src/main/java/com/example/financewallet/data/database/AssetListConverter.kt
+++ app/src/main/java/com/example/financewallet/data/database/AssetListConverter.kt
@@ -4,28 +4,18 @@ import androidx.room.TypeConverter
 import com.example.financewallet.domain.entity.Asset
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 class AssetListConverter {
 
     @TypeConverter
     fun fromAssetList(assets: List<Asset>?): String? {
-        if (assets == null) {
-            return null
-        } else {
-            val gson = Gson()
-            val type = object : TypeToken<List<Asset>>() {}.type
-            return gson.toJson(assets, type)
-        }
+        return assets?.let { Json.encodeToString(it) }
     }
 
     @TypeConverter
     fun toAssetList(assetString: String?): List<Asset>? {
-        if (assetString == null) {
-            return null
-        } else {
-            val gson = Gson()
-            val type = object : TypeToken<List<Asset>>() {}.type
-            return gson.fromJson(assetString, type)
-        }
+        return assetString?.let { Json.decodeFromString(it) }
     }
 }
